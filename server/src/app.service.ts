a81ff2af64c876f8dd7965213d86d17b44bc9e89
Vincenzo Fehring <vinfehring@gmail.com>
2025-10-02 01:01:37 -0500

    [server] update health check endpoint in AppController and AppService

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealthCheck(): { status: string; timestamp: string; uptime: number } {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime()
+    };
   }
 }
