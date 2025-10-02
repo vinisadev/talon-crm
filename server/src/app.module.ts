29e68d2c1487475b4f6efb78a725134c96c0d21c
Vincenzo Fehring <vinfehring@gmail.com>
2025-10-02 01:41:33 -0500

    [server] add modules for authentication, contacts, interactions, pipelines, and reports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { InteractionsModule } from './interactions/interactions.module';
+import { PipelinesModule } from './pipelines/pipelines.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    AuthModule,
+    ContactsModule,
+    InteractionsModule,
+    PipelinesModule,
+    ReportsModule
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
